83a3bb25df04b3a2780f24b8d63235f0f7bc4dd7
John Mair <jrmair@gmail.com>
2011-07-13 03:43:56 +1200

    removed :1 from code sample to improve syntax highlighting

--- _includes/code/tryout.rb
+++ _includes/code/tryout.rb
@@ -1,9 +1,9 @@
 pry(main)> require 'grit'
 => true
 pry(main)> cd Grit
-pry(Grit):1> ls -M Blame
+pry(Grit)> ls -M Blame
 [:lines, :load_blame, :process_raw_blame]
-pry(Grit):1> show-method Blame#load_blame
+pry(Grit)> show-method Blame#load_blame
 
 From: /Users/john/.rvm/gems/ruby-1.9.2-p180/gems/grit-2.4.1/lib/grit/blame.rb @ line 15:
 Number of lines: 4
@@ -12,4 +12,4 @@ def load_blame
   output = @repo.git.blame({'p' => true}, @commit, '--', @file)
   process_raw_blame(output)
 end
-pry(Grit):1>
+pry(Grit)>
