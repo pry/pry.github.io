17551dee0df3c2f5bf03120f3f2f197209f53bb4
Jordon Bedwell <jordon@envygeeks.com>
2012-03-22 09:08:54 -0500

    Update the example to fix #494 on Pry/pry.

--- _includes/code/tryout.rb
+++ _includes/code/tryout.rb
@@ -1,16 +1,18 @@
-pry(main)> require 'pathname'
-=> true
-pry(main)> cd Pathname
-pry(Pathname):1> show-method unlink
+pry(main)> cd FileUtils
+pry(Pathname):1> show-method rm
 
-From: /Users/john/.rvm/rubies/ruby-1.9.2-p180/lib/ruby/1.9.1/pathname.rb @ line 1031:
-Number of lines: 7
+From: /opt/ruby/lib/ruby/1.9.1/fileutils.rb @ line 556:
+Number of lines: 10
+Owner: FileUtils
 
-def unlink()
-  begin
-    Dir.unlink @path
-  rescue Errno::ENOTDIR
-    File.unlink @path
+def rm(list, options = {})
+  fu_check_options options, OPT_TABLE['rm']
+  list = fu_list(list)
+  fu_output_message "rm#{options[:force] ? ' -f' : ''} #{list.join ' '}" if options[:verbose]
+  return if options[:noop]
+
+  list.each do |path|
+    remove_file path, options[:force]
   end
 end
-pry(Pathname):1>
+pry(Pathname):2>
